224eb69664c2934861185ce14605beaf0e61cdb4
MaxChang3 <maxcfoo@gmail.com>
2024-09-22 11:35:45 +0800

    chore: add alias for src directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     plugins: [tsconfigPaths()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
