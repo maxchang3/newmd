89fd9010d848e2c80b809c33f64cc08d8c028ba0
MaxChang3 <maxcfoo@gmail.com>
2025-03-07 02:55:49 +0800

    refactor: simplify frontmatter generation logic

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import yaml from 'js-yaml'
 import TOML from 'smol-toml'
 
 export const getFrontmatter = (object: Record<string, unknown>, toml: boolean) => {
-    if (toml) return `+++\n${TOML.stringify(object)}\n+++`
-    return `---\n${yaml.dump(object)}---`
+    const fence = toml ? '+++' : '---'
+    const frontmatter = toml ? TOML.stringify(object) : yaml.dump(object)
+    return `${fence}\n${frontmatter}${fence}\n`
 }
