2d25db26329734a537ad198ed35d91868ba0d403
MaxChang3 <maxcfoo@gmail.com>
2025-03-08 02:07:37 +0800

    feat: add `Builtins.VersionCommand` and `Builtins.DefinitionsCommand` to commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,8 @@
+import { Builtins } from 'clipanion'
 import { CreateCommand } from './create'
 
 export const commands = [
     CreateCommand,
+    Builtins.VersionCommand,
+    Builtins.DefinitionsCommand,
 ]
