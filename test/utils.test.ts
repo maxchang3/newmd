0a23afd112014604411250a8b8fe6ff28c9ed7ec
MaxChang3 <maxcfoo@gmail.com>
2025-03-10 01:51:58 +0800

    feat: add in-memory file system mocks and update tests for markdown file writing

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,15 @@
-import { generateFrontmatter, generateFrontmatterFromSchema } from '@/utils'
-import { describe, expect, it } from 'vitest'
+import { generateFrontmatter, generateFrontmatterFromSchema, writeMarkdownFile } from '@/utils'
+import { vol } from 'memfs'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { z } from 'zod'
+
+vi.mock('node:fs')
+vi.mock('node:fs/promises')
+
+beforeEach(() => {
+    // reset the state of in-memory fs
+    vol.reset()
+})
 
 describe('utils', () => {
     it('should return frontmatter in yaml format', () => {
@@ -32,5 +41,19 @@ describe('utils', () => {
             titleKey: 'headline',
         })
         expect(frontmatter).toBe('---\nheadline: Hello World\n---\n')
+    })
+
+    it('should write markdown file', async () => {
+        const filename = 'hello-world'
+        const content = 'Hello World'
+        const filepath = await writeMarkdownFile({
+            filename,
+            content,
+            path: '/',
+            cwd: '/',
+        })
+        expect(filepath).toBe(`/${filename}.md`)
+        expect(vol.readdirSync('/')).toContain(`${filename}.md`)
+        expect(vol.readFileSync(filepath, 'utf-8')).toBe(content)
     })
 })
