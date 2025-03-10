ae4429cdb5a0379bdf6b0ecb7c08beef6e8a4c64
MaxChang3 <maxcfoo@gmail.com>
2025-03-10 10:44:30 +0800

    test: add tests for config resolution

---
+++ test/config.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest'
+import { resolveConfig } from "@/config";
+import { resolve } from 'pathe'
+
+const fixtureDir = resolve(__dirname, 'fixtures')
+
+describe('config', () => {
+
+    it('should return default config', async () => {
+        const config = await resolveConfig()
+        expect(config).matchSnapshot()
+    })
+
+    it('should return merged config', async () => {
+        const cwd = resolve(fixtureDir, 'headline')
+        const config = await resolveConfig({}, { cwd })
+        expect(config).matchSnapshot()
+    })
+})
+
---
+++ test/fixtures/headline/newmd.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "@/types"
+import { z } from "zod"
+
+export default ({
+    titleMapping: 'headline',
+    schemas: {
+        blog: z.object({
+            heading: z.string(),
+        })
+    }
+}) as Config
